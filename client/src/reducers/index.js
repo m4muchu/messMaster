9c6cb5042f56277f5bd36441c6193bcb537a58dd
m4muchu <muchu.pkd123@gmail.com>
2018-09-06 17:13:03 +0530

    testing

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import messCutHistoryReducer from './messCutHistoryReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  mess: messCutHistoryReducer
-});
--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//   mongoURI: 'mongodb://obcadmin:yahiya2018@ds139341.mlab.com:39341/obc',
-//   secretOrKey: 'messmasteryahiyashijil'
-// };
+module.exports = {
+  mongoURI: 'mongodb://obcadmin:yahiya2018@ds139341.mlab.com:39341/obc',
+  secretOrKey: 'messmasteryahiyashijil'
+};
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
